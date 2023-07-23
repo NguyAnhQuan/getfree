ec2da360b6a7eaee8016bdb7e9691948f50f935b
Nguyen Anh Quan <anhq46724@gmail.com>
2023-07-23 13:40:14 +0700

    first commit

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón đi uống bia nhé.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Hê sờ lô!',
+    introDesc: `Bạn thật sự chắc chắn
+    là mình muốn nhận free chứ 👏`,
+    btnIntro: 'Chắc chắn',
+    title: '',
+    desc: ' ',
+    btnYes: 'Inbox cho Quân',
+    btnNo: 'Lấy free chứ :3',
+    question:'',
+    btnReply: 'Gửi cho Quân :3',
+    reply: '',
+    mess: 'Quân đã nhận được tin nhắn .',
+    messDesc: 'Vui lòng chuyển sang mess.',
+    btnAccept: 'Okiiiii lun :3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function firstQuestion(){
         imageUrl: 'img/lookMe.jpg',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/cuteCat.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
       }).then(function(){
@@ -48,7 +48,7 @@ function firstQuestion(){
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('sound/Swish1.mp3');
+    var audio = new Audio('sound/duck.mp3');
     audio.play();
     var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
     var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
@@ -73,26 +73,26 @@ $('#no').click(() => {
         switchButton();
 })
 
-// generate text in input
-function textGenerate() {
-    var n = "";
-    var text = " " + CONFIG.reply;
-    var a = Array.from(text);
-    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
-    var count = textVal.length;
-    if (count > 0) {
-        for (let i = 1; i <= count; i++) {
-            n = n + a[i];
-            if (i == text.length + 1) {
-                $('#txtReason').val("");
-                n = "";
-                break;
-            }
-        }
-    }
-    $('#txtReason').val(n);
-    setTimeout("textGenerate()", 1);
-}
+// // generate text in input
+// function textGenerate() {
+//     var n = "";
+//     var text = " " + CONFIG.reply;
+//     var a = Array.from(text);
+//     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+//     var count = textVal.length;
+//     if (count > 0) {
+//         for (let i = 1; i <= count; i++) {
+//             n = n + a[i];
+//             if (i == text.length + 1) {
+//                 $('#txtReason').val("");
+//                 n = "";
+//                 break;
+//             }
+//         }
+//     }
+//     $('#txtReason').val(n);
+//     setTimeout("textGenerate()", 1);
+// }
 
 // show popup
 $('#yes').click(function() {
@@ -104,7 +104,7 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/iput-bg.jpg")' ,
         backdrop: `
               rgba(0,0,123,0.4)
               url("img/giphy2.gif")
@@ -112,7 +112,7 @@ $('#yes').click(function() {
               no-repeat
             `,
         confirmButtonColor: '#3085d6',
-        confirmButtonColor: '#fe8a71',
+        confirmButtonColor: '#003366',
         confirmButtonText: CONFIG.btnReply
     }).then((result) => {
         if (result.value) {
@@ -130,4 +130,5 @@ $('#yes').click(function() {
         }
     })
 })
+
 
